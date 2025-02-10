b644a38a5c4b4fbcaa8b9e4a4e61b6f2334363b5
Sally <05s.moonlight@gmail.com>
2025-02-10 18:02:48 +0800

    Update menuScreens.h

--- menuScreens.h
+++ menuScreens.h
@@ -9,12 +9,8 @@
 
 using namespace std;
 
-// Function declarations (prototypes)
 void printStartScreen();
 void printPauseScreen();
-
-// bool loadGameData(int &playerX, int &playerY, int &movesLeft, int &score, int &mazeNum);
-// bool trySaveGameData(int playerX, int playerY, int movesLeft, int score, int mazeNum);
 
 bool trySaveGameData(int playerX, int playerY, int movesLeft, int score, int mazeNum, const vector<vector<string>>& currentMaze);
 bool loadGameData(int& playerX, int& playerY, int& movesLeft, int& score, int& mazeNum, vector<vector<string>>& currentMaze);
